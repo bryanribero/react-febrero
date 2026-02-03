c8ade76e13762d5b7810f55886d9c680bf1d0d79
Bryan <bryanribero@hotmail.com>
2026-02-03 00:29:37 -0300

    Creacion del contexto theme e implementado en el componente box para practicar

---
+++ src/component/Box.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useContext } from 'react'
+import { ThemeContext } from '../context/ThemeContext'
+import '../css/boxStyle.css'
+
+export default function Box() {
+  const { theme, toggleTheme } = useContext(ThemeContext)
+  return (
+    <div>
+      <p className={theme}>Hola gente, este es un contexto</p>
+      <button onClick={toggleTheme}>Cambio de clase</button>
+    </div>
+  )
+}
---
+++ src/context/ThemeProvider.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { ThemeContext } from './ThemeContext'
+
+export function ThemeProvider({ children }) {
+  const [theme, SetTheme] = useState('Light')
+
+  function toggleTheme() {
+    SetTheme((prev) => (prev === 'Light' ? 'Black' : 'Light'))
+  }
+
+  return <ThemeContext.Provider value={{ theme, toggleTheme }}>{children}</ThemeContext.Provider>
+}
