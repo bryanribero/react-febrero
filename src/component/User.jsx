620cd0816c7ffd24127033c5b01f59a41b724085
Bryan <bryanribero@hotmail.com>
2026-02-06 02:39:41 -0300

    Ajustandoel style en propiedad

--- src/component/User.jsx
+++ src/component/User.jsx
@@ -4,14 +4,14 @@ import { UserContext } from '../context/UserContext.jsx'
 export default function User() {
   let { name, age, handlerName, handlerAge } = useContext(UserContext)
   return (
-    <div>
+    <div style={{ padding: '20px' }}>
       <label htmlFor="name"></label>
       <input type="text" id="name" placeholder="Name" onChange={handlerName} />
 
       <label htmlFor="age"></label>
       <input type="number" name="age" id="age" placeholder="age" onChange={handlerAge} />
 
-      <div>
+      <div style={{ padding: '20px' }}>
         <p>{name}</p>
         <p>{age}</p>
       </div>
