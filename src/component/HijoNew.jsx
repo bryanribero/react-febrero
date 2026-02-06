6d8fd0129e54e7a4ee02e39443e8554a6b6798de
Bryan <bryanribero@hotmail.com>
2026-02-06 19:03:05 -0300

    Creando props de manera desestructurada

---
+++ src/component/HijoNew.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function HijoNew({ name, age }) {
+  return (
+    <div>
+      <p>Name: {name}</p>
+      <p>Name: {age}</p>
+    </div>
+  )
+}
--- src/component/Padre.jsx
+++ src/component/Padre.jsx
@@ -1,10 +1,13 @@
 import Hijo from './Hijo'
+import HijoNew from './HijoNew'
 
 export default function Padre() {
   return (
     <div>
       <Hijo name="Bryan" age={27} />
       <Hijo name="Mauro" age={15} />
+
+      <HijoNew name="Maria" age={33} />
     </div>
   )
 }
