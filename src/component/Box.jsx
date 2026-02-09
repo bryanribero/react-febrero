3b1fd73b213e43163f17be77f454ff3abe5f9375
Bryan <bryanribero@hotmail.com>
2026-02-09 03:30:11 -0300

    Viendo css module

--- src/component/Box.jsx
+++ src/component/Box.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { useContext } from 'react'
-import '../css/boxStyle.css'
-import { ThemeContext } from '../Context/ThemeContext'
+import Style from '../css/boxStyle.module.css'
+import { ThemeContext } from '../context/ThemeContext'
 
 export default function Box() {
   const { theme, toggleTheme } = useContext(ThemeContext)
   return (
     <div>
-      <p className={theme}>Hola gente, este es un contexto</p>
+      <p className={Style[theme]}>Hola gente, este es un contexto</p>
       <button onClick={toggleTheme}>Cambio de clase</button>
     </div>
   )
